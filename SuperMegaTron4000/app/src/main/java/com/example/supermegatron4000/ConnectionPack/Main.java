c350adf76a2746c43ceb2a1d1193c84c1110aaf1
MohawkPigeon <42830998+MohawkPigeon@users.noreply.github.com>
2020-06-03 11:44:45 +0200

    main udkommenteret

--- SuperMegaTron4000/app/src/main/java/com/example/supermegatron4000/ConnectionPack/Main.java
+++ SuperMegaTron4000/app/src/main/java/com/example/supermegatron4000/ConnectionPack/Main.java
@@ -7,8 +7,8 @@ import com.example.supermegatron4000.model.myRoom;
 import java.util.List;
 public class Main {
     public static void main(String[] args) {
-        ConnectionManager cm = new ConnectionManager();
-        List<myRoom> rs = cm.getAllRooms();
-        rs.get(0).getRoomName();
+        //ConnectionManager cm = new ConnectionManager();
+        //List<myRoom> rs = cm.getAllRooms();
+        //rs.get(0).getRoomName();
     }
 }
